a31cbc7ae67f338e309ca98e299175a333f9a2ad
Enrique Sánchez Vicente <82119845+EnriqueSanVic@users.noreply.github.com>
2022-01-29 14:22:14 +0100

    enlazado infoHandler con InfoActivity

--- IPDownApp/app/src/main/java/com/example/ipdownapp/handlers/InfoHandler.java
+++ IPDownApp/app/src/main/java/com/example/ipdownapp/handlers/InfoHandler.java
@@ -1,7 +1,12 @@
 package com.example.ipdownapp.handlers;
 
+import com.example.ipdownapp.activitys.InfoActivity;
+
 public class InfoHandler {
 
+    private InfoActivity vista;
 
-
+    public InfoHandler(InfoActivity vista) {
+        this.vista = vista;
+    }
 }
